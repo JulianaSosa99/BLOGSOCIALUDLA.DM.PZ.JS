34e41eaaac552245e44dcccf8733fce37dd415f0
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-06-29 10:24:24 -0500

    Funcionalidad de Login y Logout

--- WebSocialUdla/Controllers/CuentaController.cs
+++ WebSocialUdla/Controllers/CuentaController.cs
@@ -7,10 +7,13 @@ namespace WebSocialUdla.Controllers
     public class CuentaController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public CuentaController(UserManager<IdentityUser>userManager)
+        public CuentaController(UserManager<IdentityUser>userManager,
+            SignInManager<IdentityUser>signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
         [HttpGet]
         public IActionResult Registrar()
@@ -42,5 +45,33 @@ namespace WebSocialUdla.Controllers
             }
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewmodel loginViewmodel)
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(loginViewmodel.Usuario,
+                loginViewmodel.Contrasenia, false, false);
+            if (signInResult != null & signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //Mostrar errores
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    
     }
 }
---
+++ WebSocialUdla/Models/ViewModels/LoginViewmodel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebSocialUdla.Models.ViewModels
+{
+    public class LoginViewmodel
+    {
+        public string Usuario { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
